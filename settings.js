ff770c17f591d12b8266373607f7a2c7d6bcf686
bot6470 <rickmajumdar328@gmail.com>
2025-06-13 15:31:59 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: '💗⎯⎯❥ RICK ❥⎯⎯💗',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "💗⎯⎯❥ RICK ❥⎯⎯💗",
+  botOwner: '💗⎯⎯❥ RICK ❥⎯⎯💗', // Your name
+  ownerNumber: '918910051810', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
